273944935088dba63bfbab1d93cf48089d525a7b
giegeralexandra <giegeralexandra@gmail.com>
2021-05-22 16:14:12 -0400

    setup controller actions for reservations and user

--- app/controllers/api/v1/rentals_controller.rb
+++ app/controllers/api/v1/rentals_controller.rb
@@ -1,2 +1,44 @@
 class Api::V1::RentalsController < ApplicationController
+
+    # before_action :set_user 
+    #/api/v1/users/1/rentals
+
+    def index
+        @rentals = Rental.all
+        render json: @rentals 
+    end
+
+    def create
+        @rental = Rental.new(rental_params)
+        if @rental.save
+            render json: @rental 
+        else 
+            render json: {error: 'Error creating Rental'}
+        end
+    end
+
+    def show
+        @rental = Rental.find(params[:id])
+        render json: @rental 
+    end
+
+    def update 
+    end
+
+    def destroy 
+        @rental = Rental.find(params[:id])
+        @rental.destroy 
+    end
+
+    private 
+
+    def set_user
+        @owner = User.find(params[:owner_id])
+    end
+
+    def rental_params
+        params.require(:rental).permit(:name, :description, :address, :rental_type, :price, :owner_id)
+    end
+
+
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,34 @@
 class Api::V1::UsersController < ApplicationController
+
+
+    def index
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            #session login 
+            render json: @user
+        else 
+            render json: {error: 'User creation error'}
+        end
+    end
+
+    def show
+        @user = User.find(params[:id])
+        render json: @user
+    end
+
+    def destroy 
+        @user = User.find(params[:id])
+        @user.delete
+    end
+
+    private 
+
+    def user_params
+        params.require(:user, :email).permit(:name)
+
+    end
+
 end
