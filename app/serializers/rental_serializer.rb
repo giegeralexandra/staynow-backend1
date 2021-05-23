f8142b04c51526a54106138462464c9eb7038f67
giegeralexandra <giegeralexandra@gmail.com>
2021-05-22 21:39:09 -0400

    edited serializers

--- app/serializers/rental_serializer.rb
+++ app/serializers/rental_serializer.rb
@@ -1,3 +1,3 @@
 class RentalSerializer < ActiveModel::Serializer
-  attributes :id, :description, :address, :rental_type, :price, :owner_id, :owner, :reservations, :guests, :reviews
+  attributes :id, :name, :description, :address, :rental_type, :price, :owner_id, :owner, :reservations, :guests, :reviews
 end
